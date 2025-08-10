967efbac523ebae824f2e67b2996cbebe56a2239
FoodDeliveryIOSApp <mychko.mychko@gmail.com>
2025-08-11 00:19:22 +0300

    Almost finished onboarding screens

--- FoodDeliveryApp/FoodDeliveryApp/UILayer/Onboarding/OnboardingViewController.swift
+++ FoodDeliveryApp/FoodDeliveryApp/UILayer/Onboarding/OnboardingViewController.swift
@@ -1,26 +1,25 @@
-//
-//  OnboardingViewPresenter.swift
-//  FoodDeliveryApp
-//
-//  Created by .Leeerrrmmm . on 10.08.2025.
-//
-
 import UIKit
 
-// MARK: - OnboardingViewPresenter
+// MARK: - OnboardingViewController
 class OnboardingViewController: UIViewController {
     
     // MARK: - Properties
-    private var pages = [UIViewController]()
+    private var pages: [UIViewController]
     
-    //MARK: - Views
-    private var pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
-    private var pageControl = UIPageControl()
+    // MARK: - Views
+    private let pageViewController: UIPageViewController
+    private let pageControl = UIPageControl()
+    
     weak var viewOutput: OnboadringViewOutput!
     
-    init(pages: [UIViewController] = [UIViewController](), viewOutput: OnboadringViewOutput) {
+    // MARK: - Init
+    init(pages: [UIViewController], viewOutput: OnboadringViewOutput) {
         self.pages = pages
         self.viewOutput = viewOutput
+        self.pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal
+        )
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -28,11 +27,12 @@ class OnboardingViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-       setupPageViewController()
+        view.backgroundColor = .systemBackground
+        setupPageViewController()
         setupPageControl()
-        
     }
 }
 
@@ -42,57 +42,70 @@ private extension OnboardingViewController {
         pageViewController.delegate = self
         pageViewController.dataSource = self
         
-        // начинаем с первого екрана и идем анимрованно вперед
-        pageViewController.setViewControllers(
-            [pages.first!], direction: .forward, animated: true
-        )
+        if let firstPage = pages.first {
+            pageViewController.setViewControllers(
+                [firstPage],
+                direction: .forward,
+                animated: true
+            )
+        }
         
         addChild(pageViewController)
         view.addSubview(pageViewController.view)
+        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            pageViewController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            pageViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pageViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pageViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
         pageViewController.didMove(toParent: self)
     }
     
-    func setupPageControl () {
+    func setupPageControl() {
         pageControl.numberOfPages = pages.count
         pageControl.currentPage = 0
-        
         pageControl.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(pageControl)
         
+        view.addSubview(pageControl)
         
         NSLayoutConstraint.activate([
             pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            pageControl.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30)
+            pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
         ])
     }
 }
 
 // MARK: - UIPageViewControllerDataSource
 extension OnboardingViewController: UIPageViewControllerDataSource {
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
         guard let currentIndex = pages.firstIndex(of: viewController), currentIndex > 0 else {
-            return UIViewController()
+            return nil
         }
         return pages[currentIndex - 1]
     }
     
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
         guard let currentIndex = pages.firstIndex(of: viewController), currentIndex < pages.count - 1 else {
-            return UIViewController()
+            return nil
         }
         return pages[currentIndex + 1]
     }
 }
 
-    
-
 // MARK: - UIPageViewControllerDelegate
 extension OnboardingViewController: UIPageViewControllerDelegate {
-    
-    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
-        if let index = pages.firstIndex(of: pendingViewControllers.first!) {
-            
-            pageControl.currentPage = index }
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            didFinishAnimating finished: Bool,
+                            previousViewControllers: [UIViewController],
+                            transitionCompleted completed: Bool) {
+        if completed, let visibleVC = pageViewController.viewControllers?.first,
+           let index = pages.firstIndex(of: visibleVC) {
+            pageControl.currentPage = index
+        }
     }
 }
